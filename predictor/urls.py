dcefad79b5f34491d55d1b7c475f3f08ed4f2927
bhavani902 <govrayapallybhavani@gmail.com>
2022-06-18 22:39:40 +0530

    commit

--- predictor/urls.py
+++ predictor/urls.py
@@ -24,9 +24,9 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('college_predictor/',include('college_predictor.urls') ),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ]
 if settings.DEBUG:
